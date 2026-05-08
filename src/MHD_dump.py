00e1f198273d06d0b9e56702a70f913eaafdd1d4
isotope-drive <holden.kirlan-stout@colostate.edu>
2026-05-08 15:43:09 -0600

    Update MHD_dump.py

--- src/MHD_dump.py
+++ src/MHD_dump.py
@@ -1,5 +1,5 @@
 
-freedom = [""" "I don't walk, I glide" - RAP Ferriera """,
+freedom = [""" "I don't walk, I glide. I don't trip, I'm fly." - RAP Ferriera """,
  """ "Blow up your TV" - John Prine """,
  """ "Sometimes the light at the end of the tunnel is a train" - Charles Barker """,
 """ "I'm like grandma stop going to the kingdom hall them people ain't your friends anyway" -RXKNephew """]
